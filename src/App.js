36466fc0f090de3693ef6e80e20ed75700fc9346
suren <suren@LAPTOP-4U24UJNK>
2024-06-03 18:09:23 +0530

    Add Material and Icon

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import  '../src/variable.scss';
 import  '../src/styles.scss';
-import { Button } from 'react-bootstrap';
+// import { Button } from 'react-bootstrap';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
     <h1>Welcome Our First Project</h1>
-    <Button className='btn btn-primary'>Button</Button>
+    {/* <Button className='btn btn-primary'>Button</Button> */}
+    <Button variant="contained" color="primary"> Hello MUI </Button>
     </div>
   );
 }
