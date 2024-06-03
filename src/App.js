1606d10883f8119c9fa4bb14038cad8bab1dd88f
suren <suren@LAPTOP-4U24UJNK>
2024-06-03 15:41:05 +0530

    install sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import  '../src/variable.scss';
+import  '../src/styles.scss';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
     <h1>Welcome Our First Project</h1>
+    <Button className='btn btn-primary'>Primary Button</Button>
     </div>
   );
 }
