a185a7a2abf5ca367f336dea71538a77a1013118
suren <suren@LAPTOP-4U24UJNK>
2024-06-06 12:03:32 +0530

    fontadded

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import  '../src/variable.scss';
 import  '../src/styles.scss';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 // import { Button } from 'react-bootstrap';
 import Button from '@mui/material/Button';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
 function App() {
   return (
@@ -11,6 +16,7 @@ function App() {
     <h1>Welcome Our First Project</h1>
     {/* <Button className='btn btn-primary'>Button</Button> */}
     <Button variant="contained" color="primary"> Hello MUI </Button>
+    <AccessTimeIcon/>
     </div>
   );
 }
